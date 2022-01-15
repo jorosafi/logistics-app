97c85fb74b1026d88a6ba008ce0a99e157b47217
UBC Student <jorosafi@students.cs.ubc.ca>
2022-01-15 15:59:28 -0800

    deploy try 3

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const port = 3000;
 const path = require("path");
 
 const indexRouter = require("./routes/index");
-// app.use("/api", indexRouter); Taken from 304 project -- may not need
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
@@ -16,13 +14,4 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
-// app.get("/", (req, res) => {
-//   console.log("Hello World!");
-// });
-
-app.listen(port, () => {
-  console.log(`app is listening at http:localhost:${port}`);
-});
-
-// from original express-generator template
-// module.exports = app;
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
